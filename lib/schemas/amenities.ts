a0234121dd9326a9cde6b2a3a903c00e977db892
gpgoodman <ggoodman1974@gmail.com>
2025-12-09 09:34:58 -0700

    GG - added schema for ridb amentities (what they call attributes)

---
+++ lib/schemas/amenities.ts
@@ -0,0 +1,25 @@
+import {z} from "zod";
+
+export const AmenitySchema = z.object({
+    AttributeName: z.string(),
+    AttributeValue: z.string(),
+});
+
+export type Amenity = z.infer<typeof AmenitySchema>;
+
+export const AmenitiesSchema = z.object({
+    RECDATA: z.array(AmenitySchema).default([]),
+    METADATA: z.object({
+        RESULTS: z.object({
+            CURRENT_COUNT: z.number().optional(),
+            TOTAL_COUNT: z.number().optional(),
+        }),
+    }),
+    SEARCH_PARAMETERS: z.object({
+        LIMIT: z.number().optional(),
+        OFFSET: z.number().optional(),
+        QUERY: z.string().optional(),
+    })
+})
+
+export type Amenities = z.infer<typeof AmenitiesSchema>;
